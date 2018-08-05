b8a41cd81911ff579835180211940c70b6aae18e
JoanaPeixoto <joana.saraiva.peixoto@gmail.com>
2018-08-05 22:38:17 +0100

    Add reader_writer script to interact with message_queue service

---
+++ reader_writer/producer_consumer.py
@@ -0,0 +1,48 @@
+import threading
+import logging
+import Queue
+import requests
+import json
+from settings import *
+from time import sleep
+from random import randint
+import sys
+
+def setup():
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-s) %(message)s',)
+
+setup()
+
+class ProducerThread(threading.Thread):
+    def run(self):
+        with open(INPUT_FILE, 'r') as f:
+            for line in f:
+                requests.post(URL, json = {'text': line})
+                sleep(randint(0, 1))
+            requests.post(URL, json = {'text': EXIT_MESSAGE})
+        return
+
+class ConsumerThread(threading.Thread):
+    def run(self):
+        with open(OUTPUT_FILE, 'wb') as f:
+            while True:
+                request = requests.get(URL)
+                try:
+                    message = request.json()
+                    if message['text'] == 'exit':
+                        break
+                    f.write(message['text'])
+                except ValueError as e:
+                    logging.debug(e)
+            sleep(randint(0, 1))
+        return
+
+if __name__ == '__main__':
+
+    p = ProducerThread(name='Producer')
+    c = ConsumerThread(name='Consumer')
+
+    p.start()
+    c.start()
---
+++ reader_writer/settings.py
@@ -0,0 +1,7 @@
+HOST = 'http://127.0.0.1'
+PORT = 5000
+ROUTE = '/messages'
+URL = HOST + ':' + str(PORT) + ROUTE
+INPUT_FILE = 'resources/input.txt'
+OUTPUT_FILE = 'resources/output.txt'
+EXIT_MESSAGE = 'exit'
